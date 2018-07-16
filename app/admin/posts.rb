ab164cbd8c256564a1b0f88f703c9bd17558f1f8
thanapon <thanapon.archint@gmail.com>
2018-07-16 13:27:38 +0700

    add new column to active admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Post do
-permit_params :title, :body, :image, :url
+permit_params :title, :body, :image, :url, :imageurl
 
 		show do |t|
 			attributes_table do
 				row :title
 				row :body
 				row :url
+				row :imageurl
 				row :image do
 					post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "No photo yet")
 				end
@@ -17,6 +18,7 @@ permit_params :title, :body, :image, :url
 				f.input :title
 				f.input :body
 				f.input :url
+				f.input :imageurl
 				f.input :image, hint: f.post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
 			end
 			f.actions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_142424) do
+ActiveRecord::Schema.define(version: 2018_07_16_060842) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_07_14_142424) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "url"
+    t.string "imageurl"
   end
 
   create_table "users", force: :cascade do |t|
